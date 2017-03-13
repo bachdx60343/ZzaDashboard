086dd3b0edf010e01717eea7bbadd885b4ce6847
ewa <emazuroo@gmail.com>
2017-03-13 11:25:51 +0100

    Adding missing constructor of CustomerEditView
    Removing unnecessary usings

--- ZzaDashboard/Customers/CustomerEditView.xaml.cs
+++ ZzaDashboard/Customers/CustomerEditView.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Zza.Data;
-using ZzaDashboard.Services;
 
 namespace ZzaDashboard.Customers
 {
     public partial class CustomerEditView : UserControl
     {
+        public CustomerEditView()
+        {
+            InitializeComponent();
+        }
     }
 }
