68182e4b12b541c3232ae261f0e04198786f6f58
ewa <emazuroo@gmail.com>
2017-03-14 11:21:41 +0100

    Sending alerts to the screen
    Behavior as a bridge between View and ViewModel

---
+++ ZzaDashboard/ShowNotificationMessageBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace ZzaDashboard
+{
+    public class ShowNotificationMessageBehaviour : Behavior<ContentControl>
+    {
+
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(ShowNotificationMessageBehaviour), 
+            new PropertyMetadata(null, OnMessageChanged));
+
+        private static void OnMessageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = d as ShowNotificationMessageBehaviour;
+            behavior.AssociatedObject.Content = e.NewValue;
+            behavior.AssociatedObject.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += (s, e2) =>
+            {
+                AssociatedObject.Visibility = Visibility.Collapsed;
+            };
+        }
+    }
+}
