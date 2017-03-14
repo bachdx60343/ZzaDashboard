c7913f5435d151c0c62d505864874e6a5987de6a
ewa <emazuroo@gmail.com>
2017-03-14 13:26:41 +0100

    Building MVVM hierarchy and creating BindableBase class for INPC encapsulation

---
+++ MVVMApplied/ZzaDesktop/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDesktop.Customers;
+using ZzaDesktop.OrderPrep;
+using ZzaDesktop.Orders;
+
+namespace ZzaDesktop
+{
+    class MainWindowViewModel : BindableBase
+    {
+        private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+        private OrderViewModel _orderViewModel = new OrderViewModel();
+        private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+
+        private BindableBase _currentViewModel;
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { SetProperty(ref _currentViewModel, value); }
+        }
+    }
+}
